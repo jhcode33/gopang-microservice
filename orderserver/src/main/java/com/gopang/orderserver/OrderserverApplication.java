3d73555f5ab1a3eb693b0ff0435d64cbc91af52f
jhcode33 <jhcode33@gmail.com>
2023-11-13 17:19:38 +0900

    Init: All Project

---
+++ orderserver/src/main/java/com/gopang/orderserver/OrderserverApplication.java
@@ -0,0 +1,13 @@
+package com.gopang.orderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderserverApplication.class, args);
+    }
+
+}
---
+++ orderserver/src/test/java/com/gopang/orderserver/OrderserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopang.orderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
